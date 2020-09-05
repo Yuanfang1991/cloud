56e5de8db9c6408570ba7a818e6f1b1f09364851
18391814276 <1274304237@qq.com>
2020-09-05 18:06:22 +0800

    wb

---
+++ gateway/src/main/java/cloud/biz/ctl/RouteController.java
@@ -0,0 +1,120 @@
+package cloud.biz.ctl;
+
+
+import cloud.biz.svc.DynamicRouteServiceImpl;
+import cloud.model.GatewayFilterDefinition;
+import cloud.model.GatewayPredicateDefinition;
+import cloud.model.GatewayRouteDefinition;
+import java.net.URI;
+import java.util.ArrayList;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.gateway.filter.FilterDefinition;
+import org.springframework.cloud.gateway.handler.predicate.PredicateDefinition;
+import org.springframework.cloud.gateway.route.RouteDefinition;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
+import reactor.core.publisher.Mono;
+
+/**
+ * 手动增删改，动态路由
+ */
+@RestController
+@RequestMapping("/route")
+public class RouteController {
+
+    @Autowired
+    private DynamicRouteServiceImpl dynamicRouteService;
+
+    //RouteDefinitionLocator是个接口，在org.springframework.cloud.gateway.route包下，如果想查看网关中所有的路由信息，调用此接口方法是一个办法，需要从先注入到容器
+
+    /**
+     * 查看路由信息两种方式
+     *
+     * 1、RouteDefinitionLocator是个接口，在org.springframework.cloud.gateway.route包下，
+     *   如果想查看网关中所有的路由信息，调用此接口方法是一个办法，需要从先注入到容器
+     *
+     * 2、Spring Cloud Gateway 提供了 Endpoint 端点，暴露路由信息，有获取所有路由、刷新路由、查看单个路由、删除路由等方法，
+     *    源码在 org.springframework.cloud.gateway.actuate.GatewayControllerEndpoint 中
+     *    查看路由链接：http://localhost:8002/actuator/gateway/routes
+     *
+     */
+
+    //增加路由
+    @PostMapping("/add")
+    public String add(@RequestBody GatewayRouteDefinition gwdefinition) {
+        String flag = "fail";
+        try {
+            RouteDefinition definition = assembleRouteDefinition(gwdefinition);
+            flag = this.dynamicRouteService.add(definition);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return flag;
+    }
+
+    //删除路由
+    @DeleteMapping("/routes/{id}")
+    public Mono<ResponseEntity<Object>> delete(@PathVariable String id) {
+        try {
+            return this.dynamicRouteService.delete(id);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    //更新路由
+    @PostMapping("/update")
+    public String update(@RequestBody GatewayRouteDefinition gwdefinition) {
+        RouteDefinition definition = assembleRouteDefinition(gwdefinition);
+        return this.dynamicRouteService.update(definition);
+    }
+
+    //把传递进来的参数转换成路由对象
+    private RouteDefinition assembleRouteDefinition(GatewayRouteDefinition gwdefinition) {
+        RouteDefinition definition = new RouteDefinition();
+        definition.setId(gwdefinition.getId());
+        definition.setOrder(gwdefinition.getOrder());
+
+        //设置断言
+        List<PredicateDefinition> pdList=new ArrayList<>();
+        List<GatewayPredicateDefinition> gatewayPredicateDefinitionList=gwdefinition.getPredicates();
+        for (GatewayPredicateDefinition gpDefinition: gatewayPredicateDefinitionList) {
+            PredicateDefinition predicate = new PredicateDefinition();
+            predicate.setArgs(gpDefinition.getArgs());
+            predicate.setName(gpDefinition.getName());
+            pdList.add(predicate);
+        }
+        definition.setPredicates(pdList);
+
+        //设置过滤器
+        List<FilterDefinition> filters = new ArrayList();
+        List<GatewayFilterDefinition> gatewayFilters = gwdefinition.getFilters();
+        for(GatewayFilterDefinition filterDefinition : gatewayFilters){
+            FilterDefinition filter = new FilterDefinition();
+            filter.setName(filterDefinition.getName());
+            filter.setArgs(filterDefinition.getArgs());
+            filters.add(filter);
+        }
+        definition.setFilters(filters);
+
+        URI uri = null;
+        if(gwdefinition.getUri().startsWith("http")){
+            uri = UriComponentsBuilder.fromHttpUrl(gwdefinition.getUri()).build().toUri();
+        }else{
+            // uri为 lb://consumer-service 时使用下面的方法
+            uri = URI.create(gwdefinition.getUri());
+        }
+        definition.setUri(uri);
+        return definition;
+    }
+
+
+}
